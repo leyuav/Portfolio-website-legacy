a4ff4301306d1cfa055fcb4f1533518094e4e96a
leyuav <leyu.av@gmail.com>
2024-06-05 19:29:42 +0200

    Fix code position

--- src/scripts.js
+++ src/scripts.js
@@ -44,18 +44,17 @@ document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('resize', function () {
     navbarLinks.style.display = (window.innerWidth > 1050) ? 'flex' : 'none';
   });
+
+  // Project-boxes temporary alert
+  const projectBoxes = document.querySelectorAll('.project-box');
+
+  // Loop through all project boxes except the first and third
+  projectBoxes.forEach((box, index) => {
+    if (index !== 0 && index !== 2) {
+      box.addEventListener('click', function (event) {
+        alert('Not available. Work in progress.');
+        event.preventDefault(); // Prevent the default action of the <a> link
+      });
+    }
+  });
 });
-
-// Project-boxes temporary alert
-const projectBoxes = document.querySelectorAll('.project-box');
-
-// Loop through all project boxes except the first and third
-projectBoxes.forEach((box, index) => {
-  if (index !== 0 && index !== 2) {
-    box.addEventListener('click', function (event) {
-      alert('Not available. Work in progress.');
-      event.preventDefault(); // Prevent the default action of the <a> link
-    });
-  }
-});
-
