2a4e3209e2673a98b2754d87d6d98a3e847e9469
leyuav <leyu.av@gmail.com>
2024-06-09 14:33:47 +0200

    Updated project boxes

--- src/scripts.js
+++ src/scripts.js
@@ -1,58 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Move to the top button functionality
   const mybutton = document.getElementById("myBtn");
 
   window.onscroll = function () {
-    mybutton.style.display = (window.scrollY > 1500) ? "flex" : "none";
+    mybutton.style.display = window.scrollY > 1500 ? "flex" : "none";
   };
 
-  mybutton.addEventListener('click', function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  mybutton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   });
 
   // FAQ icon and panel toggle functionality
-  document.querySelectorAll('.accordion').forEach(accordion => {
-    accordion.addEventListener('click', function () {
-      this.classList.toggle('active');
-      this.parentElement.classList.toggle('active');
+  document.querySelectorAll(".accordion").forEach((accordion) => {
+    accordion.addEventListener("click", function () {
+      this.classList.toggle("active");
+      this.parentElement.classList.toggle("active");
 
-      const icon = this.querySelector('i');
-      icon.classList.toggle('fa-chevron-down');
-      icon.classList.toggle('fa-chevron-up');
+      const icon = this.querySelector("i");
+      icon.classList.toggle("fa-chevron-down");
+      icon.classList.toggle("fa-chevron-up");
 
       const answer = this.nextElementSibling;
-      answer.style.display = (answer.style.display === "block") ? "none" : "block";
+      answer.style.display =
+        answer.style.display === "block" ? "none" : "block";
     });
   });
 
   // Hamburger menu functionality
-  const hamburgerMenu = document.getElementById('hamburger-menu');
-  const navbarLinks = document.getElementById('navbar-links');
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const navbarLinks = document.getElementById("navbar-links");
 
-  hamburgerMenu.addEventListener('click', function () {
-    if (navbarLinks.style.display === 'flex') {
-      navbarLinks.style.display = 'none';
-      hamburgerMenu.innerHTML = '&#9776;';
-      this.classList.remove('cross');
+  hamburgerMenu.addEventListener("click", function () {
+    if (navbarLinks.style.display === "flex") {
+      navbarLinks.style.display = "none";
+      hamburgerMenu.innerHTML = "&#9776;";
+      this.classList.remove("cross");
     } else {
-      navbarLinks.style.display = 'flex';
-      hamburgerMenu.innerHTML = '&#10005;';
-      this.classList.add('cross');
+      navbarLinks.style.display = "flex";
+      hamburgerMenu.innerHTML = "&#10005;";
+      this.classList.add("cross");
     }
   });
 
-  window.addEventListener('resize', function () {
-    navbarLinks.style.display = (window.innerWidth > 1050) ? 'flex' : 'none';
+  window.addEventListener("resize", function () {
+    navbarLinks.style.display = window.innerWidth > 1050 ? "flex" : "none";
   });
 
   // Project-boxes temporary alert
-  const projectBoxes = document.querySelectorAll('.project-box');
+  const projectBoxes = document.querySelectorAll(".project-box");
 
   // Loop through all project boxes except the first and third
   projectBoxes.forEach((box, index) => {
-    if (index !== 0 && index !== 2) {
-      box.addEventListener('click', function (event) {
-        alert('Not available. Work in progress.');
+    if (index !== 0 && index !== 2 && index !== 4) {
+      box.addEventListener("click", function (event) {
+        alert("Not available. Work in progress.");
         event.preventDefault(); // Prevent the default action of the <a> link
       });
     }
